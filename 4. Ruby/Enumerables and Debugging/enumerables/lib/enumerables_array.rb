4be2a0de7ef46d1e9c2807edf9811f0e2a3ae422
mi-ward <mwardcoding+gh@gmail.com>
2021-09-04 17:29:41 -0400

    added AA solutions

--- 4. Ruby/Enumerables and Debugging/enumerables/lib/enumerables_array.rb
+++ 4. Ruby/Enumerables and Debugging/enumerables/lib/enumerables_array.rb
@@ -8,6 +8,16 @@ class Array
     return self
   end
 
+#AA Solution
+# def my_each(&prc)
+#   self.length.times do |i|
+#     prc.call(self|i|)
+#   end
+#
+#   self
+# end
+
+
   def my_select(&block)
     new_arr = []
     self.my_each do |el|
@@ -16,21 +26,62 @@ class Array
     new_arr
   end
 
+# AA Solution
+# def my_select(&prc)
+#   selects = []
+
+#   self.my_each do |item|
+#     selects << item if prc.call(item)
+#   end
+
+#   selects
+# end
+
+
   def my_reject(&block)
     new_arr = []
     self.each { |el| new_arr << el if !block.call(el)}
     new_arr
   end
 
+# AA Solution
+# def my_reject(&prc)
+#   selects = []
+#
+#   self.my_each do |item|
+#     selects << item unless prc.call(item)
+#   end
+#
+#   selects
+# end
+
   def my_any?(&block)
     self.each { |el| return true if block.call(el)}
     false
   end
 
+# AA Solution
+# def my_any?(&prc)
+#   self.my_each do |item|
+#     return true if prc.call(item)
+#   end
+#
+#   false
+# end
+
   def my_all?(&block)
     self.each {|el| return false if !block.call(el)}
     true
   end
+
+# AA Solution
+# def my_all?(&prc)
+#   self.my_each do |item|
+#     return true unless prc.call(item)
+#   end
+#  
+#  true
+# end
 
   def my_flatten
     flattened = []
@@ -44,6 +95,18 @@ class Array
     end
       flattened
   end
+
+# AA Solution
+#  def my_flatten
+#   flattened = []
+#   
+#  self.my_each do |el|
+#     if el.is_a?(Array)
+#       flattened.concat(el.my_flatten)
+#     else
+#       flattened << el
+#     end
+#  end
 
   def my_zip(*args)
     new_arr = Array.new(self.length) {Array.new(args.length + 1) {|i| i = nil}}
@@ -64,6 +127,23 @@ class Array
     new_arr
   end
 
+# AA Solution
+# def my_zip(*arrays)
+#   zipped = []
+
+#   self.length.times do |i|
+#     subzip = [self[i]]
+
+#     arrays.my_each do |array|
+#       subzip << array[i]
+#     end
+
+#     zipped << subzip
+#   end
+
+#   zipped
+# end
+
   def my_rotate(num=1)
     new_arr = []
     self.each { |i| new_arr << i}
@@ -71,6 +151,12 @@ class Array
     num.times {new_arr.push(new_arr.shift)}
     new_arr
   end
+
+# def my_rotate(positions = 1)
+#   split_idx = positions % self.length
+
+#   self.drop(split_idx) + self.take(split_idx)
+# end
 
   def my_join(str="")
     my_str = ""
@@ -82,6 +168,18 @@ class Array
     my_str
   end
 
+  # AA Solution
+  # def my_join(separator = "")
+  #   join = ""
+  # 
+  #   self.length.times do |i|
+  #     join += self[i]
+  #     join += separator unless i == self.length - 1
+  #   end
+  #
+  #   join
+  # end
+
   def my_reverse
     new_arr = []
 
@@ -91,6 +189,16 @@ class Array
 
     new_arr
   end
+
+# AA Solution
+# def my_reverse
+#   reversed = []
+#   self.my_each do |el|
+#     reversed.unshift(el)
+#   end
+#   
+#   reversed
+# end
 
   def bubble_sort!(&prc)
     prc ||= Proc.new {|a,b| a <=> b}
