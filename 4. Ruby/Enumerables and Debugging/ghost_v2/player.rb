0a59026f0e5e91e50993355b35ea298b819fa1c8
mi-ward <mwardcoding+gh@gmail.com>
2021-09-05 15:17:45 -0400

    Ghost Project v1 & v2

---
+++ 4. Ruby/Enumerables and Debugging/ghost_v2/player.rb
@@ -0,0 +1,20 @@
+class Player
+  
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+  end
+
+  def guess
+    puts "#{@name} - Choose a letter to add to the word:"
+    print "Guess: "
+    char = gets.chomp.downcase
+    char
+  end
+
+  def alert_invalid_guess
+    puts "Not a valid guess. Try again:"
+  end
+
+end
